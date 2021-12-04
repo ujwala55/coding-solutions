10a156e832c4205cad4b82a942d69a7c5eef888d
ujwala <83896085+ujwala55@users.noreply.github.com>
2021-12-04 18:19:21 +0530

    Create arrayintersection.cpp

---
+++ cpp files/arrayintersection.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define FOR(n1) for(int i=0;i<n1;i++) cin>>a[i]
+#define f(n2) for(int i=0;i<n2;i++) cin>>b[i]
+
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a[10],b[10],n1,n2;
+	cin>>n1>>n2;
+	FOR(n1);
+	f(n2);
+	int size=n1+n2;
+	for(int i=0;i<n1;i++)
+	{
+		for(int j=0;j<n2;j++)
+		{
+			if(a[i]==b[j])
+			{
+			  cout<<b[j]<<" ";
+			}
+		}
+	}
+	
+	return 0;
+}
