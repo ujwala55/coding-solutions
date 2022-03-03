0323c329a6375f07f484ff0ba71e881935e994d6
ujwala <83896085+ujwala55@users.noreply.github.com>
2022-03-03 21:54:29 +0530

    Create countInversion.java

---
+++ INFYTQ Prep/countInversion.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class countInversion
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+       	int count=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				count++;
+			}
+		}
+		System.out.print(count);
+    }
+}
